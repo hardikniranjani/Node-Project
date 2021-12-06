429d070e47caffaaefff95670cde3c5c09cb1972
hardikniranjani <hardikpniranjanice@gmail.com>
2021-12-06 19:03:10 +0530

    model

--- domain/user.domain.js
+++ domain/user.domain.js
@@ -1,15 +1,88 @@
-const UserModel = require('../models/Users/user.model');
-const watchHistory = require('../models/Users/watchHistory.model');
-const watchLater = require('../models/Users/watchLater.model');
-const wishlist = require('../models/Users/wishlist.model');
-
+const UserModel = require("../models/Users/user.model");
+const watchHistory = require("../models/Users/watchHistory.model");
+const watchLater = require("../models/Users/watchLater.model");
+const wishlist = require("../models/Users/wishlist.model");
 
 class UserDomain {
-    async createAnUser(req,res){
-        const user = req.User;
-        const { error } = UserModel.userValidation(user);
+
+    // create Admin 
+    async createAnAdmin(req, res) {
+        const Admin = req.body;
+        const { error } = UserModel.userValidation(Admin);
+
+        if(error) return res.send("error");
         
     }
+
+
+  // create new user domain
+  async createAnUser(req, res) {
+    const user = req.body;
+    const { error } = UserModel.userValidation(user);
+  }
+
+  // get all users
+  async getAllUsers(req, res) {
+    const result = await UserModel.find();
+
+    if (result.Role === "admin") {
+      if (result && result.IsActive === true) {
+        res.send(result);
+      } else {
+        res.status(404).send("Can't find User");
+      }
+    } else {
+      res.send("Access denied");
+    }
+  }
+
+  // get all deleted users
+
+  async getAllDeletedUsers(req, res) {
+    const result = await UserModel.find();
+    if (result.Role === "admin") {
+      if (result && result.IsActive === false) {
+        res.send(result);
+      } else {
+        res.status(404).send("Can't find User");
+      }
+    } else {
+      res.send("Access denied");
+    }
+  }
+
+  // ger user by Id
+  async getAnUser(req, res) {
+    var id = req.params.id;
+
+    const result = await UserModel.findById(id);
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(404).send("Can't find User");
+    }
+  }
+
+  // delete user by id
+  async deleteAnUser(req, res) {
+    var id = req.params.id;
+
+    const result = await UserModel.findById(id);
+    if (result.Role === "admin") {
+      if (result) {
+        await UserModel.findByIdAndUpdate(id, {
+          $set: {
+            IsActive: false,
+          },
+        });
+        res.send("Successfully deleted");
+      } else {
+        res.status(404).send("Can't find User");
+      }
+    } else {
+      res.send("Access denied");
+    }
+  }
 }
 
 module.exports = User;
