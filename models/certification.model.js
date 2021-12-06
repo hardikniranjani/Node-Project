b5891912bed70e41a8991787f0db3d8519d4d1c9
DilipKumavat <officialkumavat@gmail.com>
2021-12-06 17:21:31 +0530

    certification model

--- models/certification.model.js
+++ models/certification.model.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoDB = "mongodb://localhost/backend";
-
-mongoose
-  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"));
-
 const certificaionSchema  = new mongoose.Schema({
     TvCertificates : {
         type : Object
